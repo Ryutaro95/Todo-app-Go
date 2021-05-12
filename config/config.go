d18bf14dd4fa03b19268849fc95a1f230787ed6a
Ryutaro95 <ryutaro9595@gmail.com>
2021-05-12 22:08:18 +0900

    Setting config

---
+++ config/config.go
@@ -0,0 +1,36 @@
+package config
+
+import (
+	"log"
+
+	"gopkg.in/go-ini/ini.v1"
+)
+
+type ConfigList struct {
+	Port      string
+	SQLDriver string
+	DbName    string
+	LogFile   string
+}
+
+// ConfigListを外部から呼び出せるようグローバルで宣言する
+var Config ConfigList
+
+// main()関数より先に実行されるinit()関数を定義して ConfigList を読み込む
+func init() {
+	LoadConfig()
+}
+
+func LoadConfig() {
+	cfg, err := ini.Load("config.ini")
+	if err != nil {
+		log.Fatalln(err)
+	}
+	// config.iniに設定した値を読み込む
+	Config = ConfigList{
+		Port:      cfg.Section("web").Key("port").MustString("3000"),
+		SQLDriver: cfg.Section("db").Key("driver").String(),
+		DbName:    cfg.Section("db").Key("name").String(),
+		LogFile:   cfg.Section("web").Key("logfile").String(),
+	}
+}
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module example.com
 
 go 1.16
+
+require gopkg.in/go-ini/ini.v1 v1.62.0
